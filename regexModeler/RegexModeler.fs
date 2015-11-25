4d5a9473243332dd30013ebd0243db7ce2146df9
tory.lawson <tory.lawson@wolterskluwer.com>
2015-11-24 20:14:45 -0500

    First commit for quantifier feature branch.

--- regexModeler/RegexModeler.fs
+++ regexModeler/RegexModeler.fs
@@ -17,27 +17,34 @@
         | 'S' -> getRandomNonSpaceChar
         | otherwise -> raise(new Exception "Unsupported shorthand character class")
 
-    let rec processInput (inputStr: string): string =
+    let rec processInput (inputStr: string, n: int): string =
         let inputList = [for c in inputStr -> c]
+        let nextN = if n = 0 then n else n - 1
         match inputList with
             | [] -> ""
-            | ('}'::n::'{'::xs) -> processInput(chrsToString xs)  
-            | (y::'c'::'\\'::xs) -> processInput(chrsToString xs) + @"^" + y.ToString().ToUpper()                           // Control characters
-            | (c2::c1::'x'::'\\'::xs) -> processInput(chrsToString xs) + "0x" + chrsToString [c1; c2]                       // 2-digit hex
-            | ('}'::c4::c3::c2::c1::'{'::x::'\\'::xs) -> processInput(chrsToString xs) + "U+" + chrsToString [c1;c2;c3;c4]  // 4-digit hex to Unicode
-            | (c4::c3::c2::c1::'u'::'\\'::xs) -> processInput(chrsToString xs) + "U+" + chrsToString[c1;c2;c3;c4]           // Unicode
-            | (x::'\\'::'\\'::xs) -> processInput(chrsToString xs) + @"\" + x.ToString()                                    // Literal slash
-            | (x::'\\'::xs) -> processInput(chrsToString xs) + (processCharClass x).ToString()                              // Shorthand char class
-            | (x::xs) -> stringAppend (processInput(chrsToString xs)) x                                                     // Non-escaped literal
+            | ('}'::n::'{'::xs) -> processInput(chrsToString xs, int <| Char.GetNumericValue n)  
+            | (y::'c'::'\\'::xs) -> processInput(chrsToString xs, nextN) + @"^" + y.ToString().ToUpper()                           // Control characters
+            | (c2::c1::'x'::'\\'::xs) -> processInput(chrsToString xs, nextN) + "0x" + chrsToString [c1; c2]                       // 2-digit hex
+            | ('}'::c4::c3::c2::c1::'{'::x::'\\'::xs) -> processInput(chrsToString xs, nextN) + "U+" + chrsToString [c1;c2;c3;c4]  // 4-digit hex to Unicode
+            | (c4::c3::c2::c1::'u'::'\\'::xs) -> processInput(chrsToString xs, nextN) + "U+" + chrsToString[c1;c2;c3;c4]           // Unicode
+            | (x::'\\'::'\\'::xs) -> processInput(chrsToString xs, nextN) + @"\" + x.ToString()                                    // Literal slash
+
+            | (x::'\\'::xs) -> if nextN = 0 then processInput(chrsToString xs, nextN)
+                               else processInput(chrsToString (x::'\\'::xs), nextN)
+                                + (processCharClass x).ToString()                           // Shorthand char class            
+            | (x::'\\'::xs) -> if nextN = 0 then processInput(chrsToString xs, nextN) + (processCharClass x).ToString() 
+                               else processInput(chrsToString (x::'\\'::xs), nextN) + (processCharClass x).ToString()                           // Shorthand char class
+            | (x::xs) -> if nextN = 0 then stringAppend (processInput(chrsToString xs, nextN)) x   
+                         else stringAppend (processInput(chrsToString (x::xs), nextN)) x                                           // Non-escaped literal
             
                 
     let processUnRevInput (inputStr: string): string =
-        processInput (new string(Array.rev(inputStr.ToCharArray())))
+        processInput (new string(Array.rev(inputStr.ToCharArray())), 0)
 
     [<EntryPoint>]
     let main argv = 
         let revInput = new string(Array.rev(argv.[0].ToCharArray()))
-        Console.WriteLine(processInput revInput)
+        Console.WriteLine(processInput (revInput, 0))
         0 // return an integer exit code
 
     
--- regexModelerTests/QuantifierTests.fs
+++ regexModelerTests/QuantifierTests.fs
@@ -1,6 +1,7 @@
 ﻿namespace regexModelerTests
 
 open NUnit.Framework
+open System.Text.RegularExpressions
 open RegexModeler
 
 type QuantifierTests () = 
@@ -10,3 +11,10 @@ type QuantifierTests () =
         let expected = "heLLo"
         let testRegex = "heL{2}o"
         Assert.AreEqual(expected, processUnRevInput testRegex)
+
+    [<Test>]
+    member self.processInput_WhenGivenEscapedCharAndQuantifier_RepeatsEscapedChar() =
+        let testRegex = "he\d{3}lo"
+        let modelString = processUnRevInput testRegex
+        let modelMatch = Regex.Match(modelString, testRegex)
+        Assert.True(modelMatch.Success)

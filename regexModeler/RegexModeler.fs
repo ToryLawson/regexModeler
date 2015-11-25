a4313054625f04eab67a92e5055fb7c6ef75fd39
Tory Lawson <tory.lawson@gmail.com>
2015-11-25 08:11:22 -0500

    Tests for quant feature

--- regexModeler/RegexModeler.fs
+++ regexModeler/RegexModeler.fs
@@ -23,19 +23,17 @@
         match inputList with
             | [] -> ""
             | ('}'::n::'{'::xs) -> processInput(chrsToString xs, int <| Char.GetNumericValue n)  
-            | (y::'c'::'\\'::xs) -> processInput(chrsToString xs, nextN) + @"^" + y.ToString().ToUpper()                           // Control characters
+            | (y::'c'::'\\'::xs) -> if nextN = 0 then processInput(chrsToString xs, nextN) + @"^" + y.ToString().ToUpper()         // Control characters
+                                    else processInput(chrsToString (y::'c'::'\\'::xs), nextN) + @"^" + y.ToString().ToUpper()
             | (c2::c1::'x'::'\\'::xs) -> processInput(chrsToString xs, nextN) + "0x" + chrsToString [c1; c2]                       // 2-digit hex
             | ('}'::c4::c3::c2::c1::'{'::x::'\\'::xs) -> processInput(chrsToString xs, nextN) + "U+" + chrsToString [c1;c2;c3;c4]  // 4-digit hex to Unicode
             | (c4::c3::c2::c1::'u'::'\\'::xs) -> processInput(chrsToString xs, nextN) + "U+" + chrsToString[c1;c2;c3;c4]           // Unicode
-            | (x::'\\'::'\\'::xs) -> processInput(chrsToString xs, nextN) + @"\" + x.ToString()                                    // Literal slash
-
-            | (x::'\\'::xs) -> if nextN = 0 then processInput(chrsToString xs, nextN)
-                               else processInput(chrsToString (x::'\\'::xs), nextN)
-                                + (processCharClass x).ToString()                           // Shorthand char class            
-            | (x::'\\'::xs) -> if nextN = 0 then processInput(chrsToString xs, nextN) + (processCharClass x).ToString() 
-                               else processInput(chrsToString (x::'\\'::xs), nextN) + (processCharClass x).ToString()                           // Shorthand char class
-            | (x::xs) -> if nextN = 0 then stringAppend (processInput(chrsToString xs, nextN)) x   
-                         else stringAppend (processInput(chrsToString (x::xs), nextN)) x                                           // Non-escaped literal
+            | (x::'\\'::'\\'::xs) -> processInput(chrsToString xs, nextN) + @"\" + x.ToString()                                    // Literal slash  
+               
+            | (x::'\\'::xs) -> if nextN = 0 then processInput(chrsToString xs, nextN) + (processCharClass x).ToString()            // Shorthand char class
+                               else processInput(chrsToString (x::'\\'::xs), nextN) + (processCharClass x).ToString()  
+            | (x::xs) -> if nextN = 0 then stringAppend (processInput(chrsToString xs, nextN)) x                                   // Non-escaped literal  
+                         else stringAppend (processInput(chrsToString (x::xs), nextN)) x
             
                 
     let processUnRevInput (inputStr: string): string =
--- regexModelerTests/QuantifierTests.fs
+++ regexModelerTests/QuantifierTests.fs
@@ -18,3 +18,19 @@ type QuantifierTests () =
         let modelString = processUnRevInput testRegex
         let modelMatch = Regex.Match(modelString, testRegex)
         Assert.True(modelMatch.Success)
+
+    // TODO: create real test cases
+    [<TestCase "he\d{3}lo">]    // digit char class
+    [<TestCase "he\d{3}lo">]    // control chars
+    [<TestCase "he\d{3}lo">]    // 4 digit hex
+    [<TestCase "he\d{3}lo">]    // 2 digit hex
+    [<TestCase "he\d{3}lo">]    // literal slash
+    [<TestCase "he\d{3}lo">]    // Unicode
+    [<TestCase "he\d{3}lo">]    // Literal character
+    member self.``When given a single quantifier and a single value, repeats value``(testRegex) =
+        let modelString = processUnRevInput testRegex
+        let modelMatch = Regex.Match(modelString, testRegex)
+        Assert.True(modelMatch.Success)
+
+
+

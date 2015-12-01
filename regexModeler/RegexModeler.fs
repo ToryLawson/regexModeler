0a63a58b107f01258e96dfe68662a3e9d5229b56
Tory Lawson <tory.lawson@gmail.com>
2015-11-30 23:07:44 -0500

    Refactored to simplify pattern match resuts in processInput.

--- regexModeler/RegexModeler.fs
+++ regexModeler/RegexModeler.fs
@@ -19,12 +19,12 @@
         match inputList with 
         | x::'\\'::'b'::y::xs ->
             if CharSets.IsNonWord x || CharSets.IsNonWord y 
-                then x :: (processWordBoundaries (y::xs))
-            else x::' ':: (processWordBoundaries (y::xs))
+                then x ::(processWordBoundaries (y::xs))
+            else x::' '::(processWordBoundaries (y::xs))
         | '\\'::'b'::xs -> 
             processWordBoundaries xs
         | x::xs -> 
-            x:: (processWordBoundaries xs)
+            x::(processWordBoundaries xs)
         | x -> x
             
     let processCharClass (ch:char) :char =
@@ -50,29 +50,21 @@
         | ('}'::n::'{'::xs) ->                                                                                          // Single quantifier
             processInput(xs, int <| Char.GetNumericValue n)     
         | ('\\'::'\\'::xs) ->                                                                                           // Literal slash, at end  
-            if nextN = 0 then processInput(xs, nextN) @ ['\\']
-                         else processInput(inputList, nextN) @ ['\\'] 
+            processInput((if nextN = 0 then xs else inputList), nextN) @ ['\\']
         | (y::'c'::'\\'::xs) ->                                                                                         // Control characters
-            if nextN = 0 then processInput(xs, nextN) @ ['^'; Char.ToUpper(y)]                      
-                         else processInput(inputList, nextN) @ ['^'; Char.ToUpper(y)]
+            processInput((if nextN = 0 then xs else inputList), nextN) @ ['^'; Char.ToUpper(y)] 
         | (c2::c1::'x'::'\\'::xs) ->                                                                                    // 2-digit hex
-            if nextN = 0 then processInput(xs, nextN) @ ['0';'x'; c1; c2]
-                         else processInput(inputList, nextN) @  ['0'; 'x'; c1; c2]
+            processInput((if nextN = 0 then xs else inputList), nextN) @ ['0';'x'; c1; c2]
         | ('}'::c4::c3::c2::c1::'{'::x::'\\'::xs) ->                                                                    // 4-digit hex to Unicode
-            if nextN = 0 then processInput(xs, nextN) @ ['U'; '+'; c1; c2; c3; c4]
-                         else processInput(inputList, nextN) @ ['U'; '+'; c1; c2; c3; c4]
+            processInput((if nextN = 0 then xs else inputList), nextN) @ ['U'; '+'; c1; c2; c3; c4]
         | (c4::c3::c2::c1::'u'::'\\'::xs) ->                                                                            // Unicode
-            if nextN = 0 then processInput(xs, nextN) @ ['U'; '+'; c1; c2; c3; c4]
-                         else processInput(inputList, nextN) @ ['U'; '+'; c1; c2; c3; c4]
+            processInput((if nextN = 0 then xs else inputList), nextN) @ ['U'; '+'; c1; c2; c3; c4]
         | (x::'\\'::'\\'::xs) ->                                                                                        // Literal slash, not at end  
-            if nextN = 0 then processInput(xs, nextN) @ ['\\'; x]
-                         else processInput(inputList, nextN) @ ['\\'; x]
+            processInput((if nextN = 0 then xs else inputList), nextN) @ ['\\'; x]
         | (x::'\\'::xs) ->                                                                                              // Shorthand char class
-            if nextN = 0 then processInput(xs, nextN) @ [processCharClass x]
-                         else processInput(inputList, nextN) @ [processCharClass x]  
+            processInput((if nextN = 0 then xs else inputList), nextN) @ [processCharClass x]
         | (x::xs) ->                                                                                                    // Non-escaped literal  
-            if nextN = 0 then processInput(xs, nextN) @ [x]                            
-                         else processInput(inputList, nextN) @ [x]
+            processInput((if nextN = 0 then xs else inputList), nextN) @ [x]      
         | x -> x
                 
     let processUnRevInput (inputStr: string): string =
